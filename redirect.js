e4f6915c3128bf8d88766aadf508fea56ae68fc4
Shivam Singh <stdnt.shivam@gmail.com>
2022-01-03 03:00:12 +0530

    Added Url Validation

--- redirect.js
+++ redirect.js
@@ -13,10 +13,12 @@ router.route('/:id').get(async (req, res) => {
             const link = result.rows[0].link;
             console.log(link);
             res.redirect(link)
-        };
-        res.status(400).json({
-            "message": "Bad Request"
-        })
+        }
+        else {
+            res.status(400).json({
+                "message": "Bad Request"
+            });
+        }
     }
     else {
         res.status(500).json({
--- routes/createLink.js
+++ routes/createLink.js
@@ -1,3 +1,4 @@
+const URL = require("url").URL;
 const db = require('../db');
 
 module.exports = async (req, res) => {
@@ -5,7 +6,13 @@ module.exports = async (req, res) => {
     const DEFAULT_SL_LENGTH = 5;
 
     const link = req.body.link;
-    const lqr = (await db.query(`select * from refs where link = '${link}';`));
+    if (!isValidUrl(link)) {
+        res.status(405).json({
+            "message": "Invalid URL. Make sure url starts from https://....."
+        });
+        return
+    }
+    const lqr = (await db.query(`SELECT * FROM refs WHERE link = '${link}';`));
     const linkGranted = (lqr.rowCount === 0);
     if (!linkGranted) {
         res.status(405).json({ "message": `Link Already Exists at https://${process.env.URL}/${lqr.rows[0].sl}` });
@@ -14,7 +21,7 @@ module.exports = async (req, res) => {
         var sl = undefined;
         while (true) {
             sl = makesl(DEFAULT_SL_LENGTH);
-            if ((await db.query(`select sl from refs where sl = '${sl}';`)).rows.length === 0) break;
+            if ((await db.query(`SELECT sl FROM refs WHERE sl = '${sl}';`)).rows.length === 0) break;
         };
         await db.query(`INSERT INTO refs VALUES ('${link}','${sl}')`);
         res.status(200).json({ "message": `Link is created at https://${process.env.URL}/${lqr.rows[0].sl}` });
@@ -31,3 +38,13 @@ const makesl = (length) => {
     }
     return result;
 }
+
+
+const isValidUrl = (s) => {
+    try {
+        new URL(s);
+        return true;
+    } catch (err) {
+        return false;
+    }
+};
