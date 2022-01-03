be9be8c3a989938186cc654fe959aa3e299c510e
Shivam Singh <stdnt.shivam@gmail.com>
2022-01-03 19:02:48 +0530

    Database bugfix

--- db.js
+++ db.js
@@ -1,13 +1,23 @@
 const { Pool } = require('pg');
 require('dotenv').config({ path: './.env' });
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: 5432,
-    ssl: process.env.DB_SSL === 1
-});
+var pool = undefined;
+const connectionString = process.env.DATABASE_URL;
+
+if (connectionString) {
+    pool = new Pool({
+        connectionString,
+    })
+}
+else {
+    pool = new Pool({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: 5432,
+        ssl: process.env.DB_SSL === 1
+    });
+}
 
 module.exports = pool;
